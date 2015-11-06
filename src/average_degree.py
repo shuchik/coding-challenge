1269ae8c58e0e36782736ff1869f941d6f1af425
shuchik <Yogesh@yogeshs-air.gateway.pace.com>
2015-11-05 22:42:20 -0800

    1. Updated source files with code. 2. Corrected filenames in run.sh 3. Added sample input and output

--- src/average_degree.py
+++ src/average_degree.py
@@ -1,1 +1,123 @@
-# example of program that calculates the average degree of hashtags
+#!/usr/bin/env python
+
+#Program that calculates the average degree of hashtags
+#Usage: python ./src/average_degree.py ./tweet_input/tweets.txt ./tweet_output/ft2.txt
+
+import re
+import datetime
+import tweets_cleaned
+import argparse
+import sys
+
+
+class TweetMetadata:
+    def __init__(self, _hashtags, _timestamp):
+        self.hashtags = _hashtags
+        self.timestamp = _timestamp
+
+    def __str__(self):
+        return "Hashtags: " + ",".join(self.hashtags) + "\n" + "Timestamp: " + str(self.timestamp) + "\n"
+
+
+def generate_graph(tweets_list):
+    """
+    :param tweets_list: tweets list
+    :return: graph represented by adjecency list
+    """
+    graph = {}
+
+    for tweet in tweets_list:
+        if len(tweet.hashtags) < 2: continue
+
+        for hashtag in tweet.hashtags:
+            myindex = tweet.hashtags.index(hashtag)
+            newlist = tweet.hashtags[:myindex] + tweet.hashtags[myindex + 1:]
+            for item in newlist:
+                graph.setdefault(hashtag, set()).add(item)
+    return graph
+
+
+def filter_tweets_sixty_seconds(tweets):
+    """
+    Get tweets within last 60 seconds
+    :param tweets: list of tweets
+    :return: subset of messages that fall within last 60 secs
+    """
+    if (len(tweets) == 0): return tweets
+    lasttimestamp = tweets[-1].timestamp
+    sixtysecondsbefore = lasttimestamp - datetime.timedelta(seconds=60)
+
+    return filter(lambda x: x.timestamp > sixtysecondsbefore, tweets)
+
+def calculate_average_degree(graph):
+    """
+    :param graph: graph of hashtags
+    :return:  average degree rounded to 2 digits after decimal
+    """
+    degrees = 0.0
+    # print(len(graph))
+    for value in graph.values():
+        degrees += len(value)
+    return "{0:.2f}".format(degrees / len(graph))
+
+
+def get_metadata(tweet_line):
+    """
+    Extract hashtags and timestamp from tweet
+    :param tweet_line: cleaned twitter message
+    :return: TweetMetadata object (list of hashtag, timestamp)
+    """
+    _hashtags = re.findall(r'#\w*', tweet_line)
+    matchObj = re.search(r'\(timestamp:\s(.*?)\)', tweet_line, re.M | re.I)
+    _timestamp = None
+    if matchObj:
+        twt_timestamp = matchObj.group(1)
+        _timestamp = datetime.datetime.strptime(twt_timestamp, '%a %b %d %H:%M:%S +0000 %Y')
+
+    return TweetMetadata(_hashtags, _timestamp)
+
+
+def process_tweets(input_file_path, output_file_path):
+    """
+    :param input_file_path: Input filename containing twitter JSON messages
+    :param output_file_path: Output file containing rolling average
+    """
+    tweets = []
+
+    # Empty out the file first
+    open(output_file_path, 'w').close()
+
+    with open(input_file_path) as input_file:
+        for line in input_file:
+            # extract relevant, clean information from input line
+            clean_text, created_timestamp, hasunicode = tweets_cleaned.clean_line(line)
+            if clean_text:
+                tweet = get_metadata(clean_text)
+                if len(tweet.hashtags) < 2: continue
+                #print tweet
+
+                tweets.append(tweet)
+                # filter according to time stamp
+                tweets = filter_tweets_sixty_seconds(tweets)
+                graph = generate_graph(tweets)
+                with open(output_file_path, "a") as output_file:
+                    output_file.write("{}\n".format(calculate_average_degree(graph)))
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("files", nargs="*")
+    args = parser.parse_args()
+
+    if len(args.files) != 2:
+        print "Incorrect number of parameters. Exiting.."
+        sys.exit()
+
+    input_file_path = args.files[0]
+    output_file_path = args.files[1]
+
+    print "Calculating average_degree...\n"
+    process_tweets(input_file_path, output_file_path)
+
+    print "Completed...\n"
--- src/tweets_cleaned.py
+++ src/tweets_cleaned.py
@@ -1,1 +1,96 @@
-# example of program that calculates the number of tweets cleaned
+#!/usr/bin/env python
+
+#Program that calculates the number of tweets cleaned
+#Usage: python ./src/tweets_cleaned.py ./tweet_input/tweets.txt ./tweet_output/ft1.txt
+
+# Imports
+import json
+import re
+import argparse
+import sys
+
+
+def clean_line(line):
+    """
+    Extract relevant text from a twitter message and Clean it.
+
+    :param line: Twitter JSON message as a single line
+    :return: cleaned text of tweet,timestamp of tweet, hasUniCode - boolean to indicate if message has Unicode
+    """
+    hasUnicode = False
+
+    # return if incorrect data
+    if '{"limit":{"track":' in line:
+        return None, None, False
+
+    tweet_json = json.loads(line)
+    # get created datetime of tweet
+    created_timestamp = tweet_json["created_at"]
+
+    # get text of tweet ; if unicode clean it
+    text = tweet_json["text"]
+
+    try:
+        text.decode('ascii')
+    except:
+        hasUnicode = True
+        clean_text = text.encode('ascii', 'ignore')
+    else:
+        clean_text = text
+
+    # replace escaped characters
+    clean_text = clean_text.replace('\\/', '/')
+    clean_text = clean_text.replace('\\\\','\\')
+    clean_text = clean_text.replace("\'", "'")
+    clean_text = clean_text.replace("\\\"", "\"")
+    clean_text = re.sub("\s+", " ", clean_text).strip()
+
+    clean_text = "{} (timestamp: {})".format(clean_text, created_timestamp)
+
+    return clean_text, created_timestamp, hasUnicode
+
+
+def process_tweets_file(input_file_path, output_file_path):
+    """
+    :param input_file_path: Input filename containing twitter JSON messages
+    :param output_file_path: Output file containing results
+    """
+    count_unicode = 0
+    outputlines = []
+
+    #read input file line by line
+    with open(input_file_path) as input_file:
+        for tweet in input_file:
+            # Construct output line
+            clean_text, created_timestamp, hasunicode = clean_line(tweet)
+            if clean_text:
+                if hasunicode: count_unicode += 1
+                outputlines.append(clean_text)
+
+    #construct file footer with unicode count
+    output_file_footer = "{} tweets contained unicode".format(count_unicode)
+    outputlines.append("\n%s" % output_file_footer)
+
+    #Write results to output file
+    with open(output_file_path, "w") as output_file:
+        for item in outputlines:
+            output_file.write("%s\n" % item)
+
+
+if __name__ == '__main__':
+    print "Starting tweets_cleaned ...\n"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("files", nargs="*")
+    args = parser.parse_args()
+
+    if len(args.files) != 2:
+        print "Incorrect number of parameters. Exiting.."
+        sys.exit()
+
+    input_file_path = args.files[0]
+    output_file_path = args.files[1]
+    #Process the input file containing Twitter JSON message, output results to output file
+    process_tweets_file(input_file_path, output_file_path)
+
+    print "Completed...\n"
